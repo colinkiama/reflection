01d6e0836d4d13ee72418a8f00af80608058f0b1
Colin Kiama <colinkiama@gmail.com>
2022-12-09 19:26:18 +0000

    Movement key events detected

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,6 +1,11 @@
 import Phaser from "phaser";
 
+type KeyMap = {
+  [key: string]: Phaser.Input.Keyboard.Key;
+};
+
 export default class Demo extends Phaser.Scene {
+  private _movementKeys!: KeyMap;
   constructor() {
     super("GameScene");
   }
@@ -11,5 +16,21 @@ export default class Demo extends Phaser.Scene {
 
   create() {
     const player = this.physics.add.image(400, 300, "player");
+    this._movementKeys = <KeyMap>this.input.keyboard.addKeys("W, S, A, D");
+  }
+
+  update() {
+    this.handleMovementKeys();
+  }
+  handleMovementKeys() {
+    // console.log("Movment keys:", this._movementKeys);
+    for (const key in this._movementKeys) {
+      // console.log("Key:", key);
+      let movementKey = this._movementKeys[key];
+      if (movementKey.isDown) {
+        console.log(`${key} is down: ${movementKey}`);
+      }
+      // console.log("Key:", key, ":", this._movementKeys[key]);
+    }
   }
 }
